d01d95df677d9c4f10dbff8522c53cf2bca08dad
Diego Ferreira <diego.ferreira@paddypowerbetfair.com>
2023-12-08 17:30:15 +0000

    :sparkles: feat(yaml): Adiciona adição de elemento ao yaml

--- exemplo_aula_yaml.py
+++ exemplo_aula_yaml.py
@@ -12,7 +12,7 @@ def escrever_arquivo_yaml(caminho_arquivo):
 
     with open(caminho_arquivo, 'w') as arquivo:
         # Escreve os dados no arquivo YAML
-        yaml.dump(dados_yaml, arquivo, default_flow_style=False)
+        yaml.dump(dados_yaml, arquivo, sort_keys=True)
 
     print(f"Arquivo YAML '{caminho_arquivo}' criado com sucesso!")
 
@@ -23,10 +23,18 @@ def ler_arquivo_yaml(caminho_arquivo):
         dados = yaml.load(arquivo, Loader=yaml.FullLoader)
 
     # Manipular os dados convertidos como um dicionário
-    print(dados)
+    return dados
 
+
+def adicionar_produto(caminho_arquivo, nome, preco, quantidade): 
+    conteudoAtual = ler_arquivo_yaml(caminho_arquivo)
+    conteudoAtual["pessoas"].append({"nome": nome, "preco": preco, "quantidade": quantidade})
+    with open(caminho_arquivo, 'a') as arquivo:
+    # Escreve os dados no arquivo YAML
+        yaml.dump(conteudoAtual, arquivo, sort_keys=True)
 
 yaml_leitura = 'exemplos_arquivos/values.yaml'
 yaml_escrita = 'exemplos_arquivos/exemplo.yaml'
 ler_arquivo_yaml(yaml_leitura)
 escrever_arquivo_yaml(yaml_escrita)
+adicionar_produto(yaml_escrita, "diego", 321312312, 3)
